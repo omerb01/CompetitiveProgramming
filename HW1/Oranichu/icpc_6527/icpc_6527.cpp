d71408a86c8217a0b0c43a919b0e08d2343a21e1
Oran Nahoum <33873115+oranichu@users.noreply.github.com>
2019-05-11 22:42:23 +0300

    Oranichu 6527.

--- HW1/Oranichu/icpc_6527/icpc_6527.cpp
+++ HW1/Oranichu/icpc_6527/icpc_6527.cpp
@@ -16,24 +16,24 @@
 #include <numeric>
 
 using namespace std;
-typedef vector<pair<int, int>> vii;
+typedef vector <pair<int, int>> vii;
 typedef long long ll;
 
-ll bin_length(ll n, vector<ll> &ones_places){
+ll bin_length(ll n, vector <ll> &ones_places) {
     ll length = 0;
     while (n > 0) {
 
-        if (n%2 == 1) {
+        if (n % 2 == 1) {
             ones_places.push_back(length);
         }
 
         length++;
-        n/=2;
+        n /= 2;
     }
     return length;
 }
 
-ll ll_pow (ll x, ll y){
+ll ll_pow(ll x, ll y) {
     ll sum = 1;
     for (ll i = 0; i < y; ++i) {
         sum *= x;
@@ -43,15 +43,15 @@ ll ll_pow (ll x, ll y){
 
 ll count_ones(ll n) {
     ll sum = 0;
-    vector<ll> ones_places;
-    for (ll i = bin_length(n, ones_places) - 1; i > 0 ; i--) {
-        if ((n>>i)%2 == 1) { // bit in location i is set.
-            sum+=(i * ll_pow(2,i-1));
+    vector <ll> ones_places;
+    for (ll i = bin_length(n, ones_places) - 1; i > 0; i--) {
+        if ((n >> i) % 2 == 1) { // bit in location i is set.
+            sum += (i * ll_pow(2, i - 1));
         } // add to sum all the one's from the closest pow of 2.
     }
 
-    for (ll i = 0 ; i<ones_places.size() ; i++) {
-        sum+= i* ll_pow(2, ones_places[ones_places.size() - i -1]);
+    for (ll i = 0; i < ones_places.size(); i++) {
+        sum += i * ll_pow(2, ones_places[ones_places.size() - i - 1]);
     } // add the number of one's after the closest pow of 2.
 
     return sum;
@@ -60,9 +60,9 @@ ll count_ones(ll n) {
 
 int main() {
 
-    ll a,b;
-    while (cin >> a >> b){
-        cout << count_ones(b+1) - count_ones(a) << endl;
+    ll a, b;
+    while (cin >> a >> b) {
+        cout << count_ones(b + 1) - count_ones(a) << endl;
     }
 
     return 0;
